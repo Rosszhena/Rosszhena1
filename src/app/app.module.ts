66ba70970245b4aef9578a54aff73123265d78fd
Maria Rosa Rosete <ross.zhena@gmail.com>
2018-11-15 12:48:40 -0600

    Creación de portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ClientsComponent } from './clients/clients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent,
+    SocialComponent,
+    NavigationComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule
